d2fad806e20dacf2eda65042bddadde176ca58fd
Stanislaw Kuczma <staskuczma@gmail.com>
2026-08-03 15:33:18 +0200

    add: inital commit w/ dockerfile

---
+++ README.md
@@ -0,0 +1,8 @@
+# Franka Panda ROS1 Workspace 
+
+
+## Setup
+
+Enter .docker and build image
+`docker build -t franka_ws .`
+then use `run.sh` to start the container. 
---
+++ run.sh
@@ -0,0 +1,33 @@
+IMAGE_NAME="franka_ws:latest"
+CONTAINER_NAME="franka_ws"
+
+DOMAIN=${CONTAINER_NAME: -2}
+
+xhost +local:root
+XAUTH=/tmp/.docker.xauth
+if [ ! -f $XAUTH ]
+then
+    xauth_list=$(xauth nlist :0 | sed -e 's/^..../ffff/')
+    if [ ! -z "$xauth_list" ]
+    then
+        echo $xauth_list | xauth -f $XAUTH nmerge -
+    else
+        touch $XAUTH
+    fi
+    chmod a+r $XAUTH
+fi
+docker stop $CONTAINER_NAME || true && docker rm $CONTAINER_NAME || true
+docker run -it \
+    --env="DISPLAY=$DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --env="ROS_DOMAIN_ID=$DOMAIN" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --env="XAUTHORITY=$XAUTH" \
+    --volume="$XAUTH:$XAUTH" \
+    --volume="/dev:/dev" \
+    --volume="$PWD/src:/catkin_ws/src/external" \
+    --privileged \
+    --network=host \
+    --name="$CONTAINER_NAME" \
+    $IMAGE_NAME \
+    /bin/bash
